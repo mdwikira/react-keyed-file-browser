46bd1c5a8d7b0945d827222c45fdc4e299a61e57
Maciej Wikira <maciej.daniel.wikira@gmail.com>
2021-07-08 13:28:20 +0200

    remove default details view

--- src/files/table.js
+++ src/files/table.js
@@ -104,81 +104,13 @@ const TableFile = flow(
 
 class RawExtendedTableFile extends RawTableFile {
   render() {
-    const {
-      isDragging, isDeleting, isRenaming, isOver, isSelected,
-      action, url, browserProps, connectDragPreview,
-      depth, size, modified,
-    } = this.props
-
-    const icon = browserProps.icons[this.getFileType()] || browserProps.icons.File
-    const inAction = (isDragging || action)
-
-    const ConfirmDeletionRenderer = browserProps.confirmDeletionRenderer
-
-    let name
-    if (!inAction && isDeleting && browserProps.selection.length === 1) {
-      name = (
-        <ConfirmDeletionRenderer
-          handleDeleteSubmit={this.handleDeleteSubmit}
-          handleFileClick={this.handleFileClick}
-          url={url}
-        >
-          {icon}
-          {this.getName()}
-        </ConfirmDeletionRenderer>
-      )
-    } else if (!inAction && isRenaming) {
-      name = (
-        <form className="renaming" onSubmit={this.handleRenameSubmit}>
-          {icon}
-          <input
-            ref={this.selectFileNameFromRef}
-            type="text"
-            value={this.state.newName}
-            onChange={this.handleNewNameChange}
-            onBlur={this.handleCancelEdit}
-            autoFocus
-          />
-        </form>
-      )
-    } else {
-      name = (
-        <a
-          href={url || '#'}
-          download="download"
-          onClick={this.handleFileClick}
-        >
-          {icon}
-          {this.getName()}
-        </a>
-      )
-    }
-
-    let draggable = (
-      <div>
-        {name}
-      </div>
-    )
-    if (typeof browserProps.moveFile === 'function') {
-      draggable = connectDragPreview(draggable)
-    }
+    const { browserProps, size } = this.props
 
     const row = (
-      <tr
-        className={ClassNames('file', {
-          pending: action,
-          dragging: isDragging,
-          dragover: isOver,
-          selected: isSelected,
-        })}
-        onClick={this.handleItemClick}
-        onDoubleClick={this.handleItemDoubleClick}
-      >
+      <tr>
         <td className="name">{this.props.name}</td>
         <td className="filename">
-          <div style={{ paddingLeft: (depth * 16) + 'px' }}>
-            {draggable}
-          </div>
+            {this.props.filename}
         </td>
         <td className="size">{fileSize(size)}</td>
         <td className="location">{this.props.location}</td>
